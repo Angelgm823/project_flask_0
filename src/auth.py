40d0339356d7df1fd28899659e036b0f809533f1
angelgm <gomezmendezangel100@gmail.com>
2025-10-26 20:03:56 -0600

    registros en la base de datos Usuarios con hash

--- src/auth.py
+++ src/auth.py
@@ -1,12 +1,26 @@
-from flask import Blueprint, render_template
+from flask import (Blueprint, render_template, request, redirect, flash, url_for)
+from .models import User
+from src import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 # vistas de los HTML
-@bp.route('/register')
+@bp.route('/register', methods=('GET', 'POST'))
 def register():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        password = request.form['password']
+        
+        user = User(nombre, generate_password_hash(password))
+        
+        user_name = User.query.filter_by(nombre = nombre).first()
+        if user_name == None:
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('auth.login'))
     return render_template('auth/register.html')
 
-@bp.route('/login')
+@bp.route('/login', methods=('GET', 'POST'))
 def login():
     return render_template('auth/login.html')
---
+++ src/models.py
@@ -0,0 +1,31 @@
+from src import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    nombre = db.Column(db.String(20), unique=True)
+    password = db.Column(db.Text, nullable = False)
+    
+    def __init__(self, nombre, password):
+        self.nombre = nombre
+        self.password = password
+        
+    def __repr__(self):
+        return f'<User: (self.nombre)>'
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    titulo = db.Column(db.String(100), nullable=False)
+    desc = db.Column(db.Text)
+    state = db.Column(db.Boolean, default= False)
+    
+    def __init__(self,created_by, titulo, desc, state):
+        self.created_by = created_by
+        self.titulo = titulo
+        self.desc = desc
+        self.state = state
+        
+        
+    def __repr__(self):
+        return f'<User: (self.titulo)>'
